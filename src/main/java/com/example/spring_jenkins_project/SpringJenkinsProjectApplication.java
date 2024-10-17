dee52a13aff77e7a2c84828e535405cb4df7d91b
Your Name <you@example.com>
2024-10-17 18:36:54 +0530

    Added my new project

---
+++ src/main/java/com/example/spring_jenkins_project/SpringJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jenkins_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+	}
+
+}
